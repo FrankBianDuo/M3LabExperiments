6943602d22d6cb27e0a5823b5e29120c6e317b91
gregstan <gregstan@umich.edu>
2023-02-26 22:26:40 -0500

    shrink avatars

---
+++ src/components/shrink_avatars.py
@@ -0,0 +1,14 @@
+import os
+from PIL import Image
+
+path = r"../assets/Avatars/Sighted Avatars"
+# path = "C:\cygwin64\home\Gregory Stanley\playground\M3LabExperiments\src\\assets\Avatars\Sighted Avatars"
+for filename in os.listdir(path):
+    image = Image.open(os.path.join(path, filename))
+    # reduce the image dimensions
+    print(image.width)
+    if image.width <= 800:
+        continue
+    image = image.resize((int(image.width * 0.5), int(image.height * 0.5)))
+    # save the resized image
+    image.save(os.path.join(path, filename))
